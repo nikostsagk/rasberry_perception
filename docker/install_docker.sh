72c0a175046d83d10510679a2e97775d900f2603
RaymondKirk <ray.tunstill@live.co.uk>
2019-10-15 16:21:20 +0200

    Added correct nvidia runtime install

--- docker/install_docker.sh
+++ docker/install_docker.sh
@@ -17,8 +17,10 @@ sudo curl -L "https://github.com/docker/compose/releases/download/1.24.1/docker-
 sudo chmod +x /usr/bin/docker-compose
 
 # Add the package repositories for GPU
+curl -s -L https://nvidia.github.io/nvidia-container-runtime/gpgkey | sudo apt-key add -
 distribution=$(. /etc/os-release;echo $ID$VERSION_ID)
-curl -s -L https://nvidia.github.io/nvidia-docker/gpgkey | sudo apt-key add -
-curl -s -L https://nvidia.github.io/nvidia-docker/$distribution/nvidia-docker.list | sudo tee /etc/apt/sources.list.d/nvidia-docker.lis
-sudo apt-get update && sudo apt-get install -y nvidia-container-toolkit
+curl -s -L https://nvidia.github.io/nvidia-container-runtime/$distribution/nvidia-container-runtime.list | \
+  sudo tee /etc/apt/sources.list.d/nvidia-container-runtime.list
+sudo apt-get update
+sudo apt-get install -y nvidia-container-toolkit
 sudo systemctl restart docker
