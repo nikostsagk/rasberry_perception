7f3e68dad6ab21c51c9bc74d53c0effcbbe1c34d
RaymondKirk <ray.tunstill@live.co.uk>
2019-11-22 18:09:02 +0000

    Changed setup.py python catkin setup field

--- setup.py
+++ setup.py
@@ -4,9 +4,9 @@ from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
 setup_args = generate_distutils_setup(
-    packages=['rasberry_perception', 'rasberry_perception_pkg', 'deep_learning_ros'],
+    packages=['rasberry_perception_pkg', 'deep_learning_ros'],
     package_dir={'': 'src'},
-    requires=['numpy', 'opencv-python', 'PyYaml', 'rospkg', 'tf', 'Pillow', 'SimpleDataTransport']
+    install_requires=['numpy', 'opencv-python', 'PyYaml', 'rospkg', 'tf', 'Pillow', 'SimpleDataTransport']
 )
 
 setup(**setup_args)
