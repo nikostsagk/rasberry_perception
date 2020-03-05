8f2ddb2dfa44b58f835b40481d4756a254afb667
RaymondKirk <ray.tunstill@live.co.uk>
2020-03-05 18:00:27 +0000

    Added visualisation publisher

--- src/rasberry_perception/detection/detection_client.py
+++ src/rasberry_perception/detection/detection_client.py
@@ -16,11 +16,13 @@ from sensor_msgs.msg import Image, CameraInfo
 
 from rasberry_perception.detection import Client, default_service_name
 from rasberry_perception.detection.utility import function_timer, WorkerTaskQueue
-from rasberry_perception.msg import Detections, Detection, RegionOfInterest, SegmentOfInterest
+from rasberry_perception.detection.visualisation import Visualiser
+from rasberry_perception.msg import Detections, Detection, RegionOfInterest, SegmentOfInterest, DetectionInfo
 
 
 class RunClientOnTopic:
-    def __init__(self, image_namespace, depth_namespace=None, score_thresh=0.5, service_name=default_service_name):
+    def __init__(self, image_namespace, depth_namespace=None, score_thresh=0.5, service_name=default_service_name,
+            vis=True):
         self._node_name = service_name + "_client"
         rospy.init_node(self._node_name, anonymous=True)
         stem = image_namespace.split('/')[-1]
@@ -31,8 +33,10 @@ class RunClientOnTopic:
 
         # Wait for connection to detection service
         self.detector = Client()
-        self.publisher_tasks = WorkerTaskQueue(num_workers=4, max_size=30)
-        rospy.on_shutdown(self.on_shutdown)
+        self.vis = vis
+        if self.vis:
+            self.publisher_tasks = WorkerTaskQueue(num_workers=1, max_size=60)
+            rospy.on_shutdown(self.on_shutdown)
 
         # Initialise publishers
         self.detections_pub = rospy.Publisher(self.namespace + "detections", Detections, queue_size=1)
@@ -80,12 +84,74 @@ class RunClientOnTopic:
         if not result.status.OKAY:
             return
 
-        # Offload the visualisation task <1ms over calling publish directly
-        self.publisher_tasks.add_task(self.publish, args, result=result)
-        # self.publish(*args, result=result)
+        self.publish_detections(*args, result=result)
 
     @function_timer.interval_logger(interval=10)
-    def publish(self, image_msg, image_info, depth_msg, depth_info, result):
+    def publish_detections(self, image_msg, image_info, depth_msg, depth_info, result):
+        """Function to publish detections based on the image data and detector result
+
+        Args:
+            image_msg (Image): The RGB image message
+            image_info (CameraInfo): The RGB camera info message
+            depth_msg (Image): The aligned depth to image_msg message
+            depth_info (CameraInfo):  The depth camera info message
+            result (GetDetectorResultsResponse):  The result of a call to the GetDetectorResults service api
+        """
+        detections = result.detections.detections
+        detections.extend(self._get_test_messages(depth_msg.height, depth_msg.width))
+
+        if not len(detections):
+            return
+
+        if self.depth_enabled and depth_msg is not None:
+            depth_image = ros_numpy.numpify(depth_msg)
+
+            fx, fy, cx, cy = depth_info.P[0], depth_info.P[5], depth_info.P[2], depth_info.P[6]
+
+            bbox_poses = PoseArray(header=depth_msg.header)  # TODO: Change frame_id and translate by tf
+            segm_poses = PoseArray(header=depth_msg.header)  # TODO: Change frame_id and translate by tf
+
+            def _get_pose(depth_roi, valid_positions, x_offset, y_offset, _fx, _fy, _cx, _cy):
+                zp = depth_roi[valid_positions] / 1000.0
+                yp = ((valid_positions[0] + y_offset) - _cy) * zp / _fy
+                xp = ((valid_positions[1] + x_offset) - _cx) * zp / _fx
+                return Pose(position=Point(np.median(xp), np.median(yp), np.median(zp)))
+
+            for detection in detections:
+                # TODO: Transform by TF and intrinsics
+                # Get localisation from bbox
+                bbox = detection.roi
+                xv, yv = np.meshgrid(np.arange(int(bbox.x1), int(bbox.x2)), np.arange(int(bbox.y1), int(bbox.y2)))
+                d_roi = depth_image[yv, xv]  # For bbox the x, y, z pos is based on median of valid depth pixels
+                valid_idx = np.where(np.logical_and(d_roi != 0, np.isfinite(d_roi)))
+                if len(valid_idx[0]) and len(valid_idx[1]):
+                    bbox_poses.poses.append(_get_pose(d_roi, valid_idx, bbox.x1, bbox.y1, fx, fy, cx, cy))
+
+                # Get localisation from segm
+                segm = detection.seg_roi
+                xv, yv = np.meshgrid(segm.x, segm.y)
+                d_roi = depth_image[yv, xv]  # For segm the x,y,z pos is based on median of detected pixels
+                valid_idx = np.where(np.logical_and(d_roi != 0, np.isfinite(d_roi)))
+                if len(valid_idx[0]) and len(valid_idx[1]):
+                    segm_poses.poses.append(_get_pose(d_roi, valid_idx, bbox.x1, bbox.y1, fx, fy, cx, cy))
+
+            # Publish pose arrays
+            self.depth_pub.publish(depth_msg)
+            self.depth_info_pub.publish(depth_info)
+            self.depth_bbox_detections_pub.publish(bbox_poses)
+            self.depth_segm_detections_pub.publish(segm_poses)
+
+        # Publish detection results
+        self.detections_pub.publish(result.detections)
+        self.image_pub.publish(image_msg)
+        self.image_info_pub.publish(image_info)
+
+        # Offload the visualisation task <1ms (takes considerable time)
+        if self.vis:
+            self.publisher_tasks.add_task(self._vis_publish, (image_msg, result,))
+
+    @function_timer.interval_logger(interval=10)
+    def _vis_publish(self, image_msg, result):
         """Publish function for service results. Meant to be offloaded to another thread.
 
         Args:
@@ -95,61 +161,44 @@ class RunClientOnTopic:
             depth_info (CameraInfo):  The depth camera info message
             result (GetDetectorResultsResponse):  The result of a call to the GetDetectorResults service api
         """
-        if self.depth_enabled and depth_msg is not None:
-            depth_image = ros_numpy.numpify(depth_msg)
-
-            detections = result.detections.detections
-            # For tests create a bbox and pixels
-            detections.append(Detection(roi=RegionOfInterest(200, 300, 250, 400),
-                                        seg_roi=SegmentOfInterest(x=[250, 250], y=[300, 400])))
-
-            fx, fy, cx, cy = depth_info.P[0], depth_info.P[5], depth_info.P[2], depth_info.P[6]
-            bbox_poses = PoseArray(header=image_msg.header)  # TODO: Change frame_id and translate by tf
-            segm_poses = PoseArray(header=image_msg.header)  # TODO: Change frame_id and translate by tf
-            for detection in detections:
-                # TODO: Transform by TF and intrinsics
-                # Get localisation from bbox
-                bbox = detection.roi
-                xv, yv = np.meshgrid(np.arange(int(bbox.x1), int(bbox.x2)), np.arange(int(bbox.y1), int(bbox.y2)))
-                d_roi = depth_image[yv, xv]  # For bbox the x, y, z pos is based on median of valid depth pixels
-                valid_idx = np.where(np.logical_and(d_roi != 0, np.isfinite(d_roi)))
-
-                if len(valid_idx):
-                    y_pos, x_pos = np.mean(valid_idx, axis=1)
-                    z_pos = np.median(d_roi[valid_idx])
-                    x_pos = ((x_pos - cx) * z_pos / fx) / 1000.0
-                    y_pos = ((y_pos - cy) * z_pos / fy) / 1000.0
-                    z_pos = z_pos / 1000.0
-                    bbox_poses.poses.append(Pose(position=Point(x=x_pos, y=y_pos, z=z_pos)))
-
-                # Get localisation from segm
-                segm = detection.seg_roi
-                xv, yv = np.meshgrid(segm.x, segm.y)
-                d_roi = depth_image[yv, xv]  # For segm the x,y,z pos is based on median of detected pixels
-                valid_idx = np.where(np.logical_and(d_roi != 0, np.isfinite(d_roi)))
-                if len(valid_idx):
-                    y_pos, x_pos = np.mean(valid_idx, axis=1)
-                    z_pos = np.median(d_roi[valid_idx])
-                    x_pos = ((x_pos - cx) * z_pos / fx) / 1000.0
-                    y_pos = ((y_pos - cy) * z_pos / fy) / 1000.0
-                    z_pos = z_pos / 1000.0
-                    segm_poses.poses.append(Pose(position=Point(x=x_pos, y=y_pos, z=z_pos)))
-
-            # Publish pose arrays
-            self.depth_pub.publish(depth_msg)
-            self.depth_info_pub.publish(depth_info)
-            self.depth_bbox_detections_pub.publish(bbox_poses)
-            self.depth_segm_detections_pub.publish(segm_poses)
+        from timeit import default_timer as timer
+        s = timer()
+        vis = Visualiser(ros_numpy.numpify(image_msg))
+        vis.draw_detections_message(result)
+        vis_image = vis.get_image(overlay_alpha=1.0)
+        vis_msg = ros_numpy.msgify(Image, vis_image, encoding="bgr8")
+        vis_msg.header = image_msg.header
+        self.detections_vis_pub.publish(vis_msg)
+        es = timer()
+        # print(es - s)
 
 
-        # Publish detection results
-        self.detections_pub.publish(result.detections)
-        self.image_pub.publish(image_msg)
-        self.image_info_pub.publish(image_info)
+    def _get_test_messages(self, max_y, max_x, n_grid=2, box_height=200, box_width=200):
+        """Internal use only. Returns a list of fake detections
 
-        # TODO: Reimplement visualisations publisher
-        # self.detections_vis_pub.publish(visualisation_image_message)
-        # TODO: Reimplement depth pose array publisher
+        Args:
+            max_y (int): Max y position (height) of the bounding boxes
+            max_x (int): Max x position (width) of the bounding boxes
+        """
+        detections = []
+        margin = 20
+        max_y -= box_height + (margin * 2)
+        max_x -= box_width + (margin * 2)
+
+        for i in range(n_grid + 1):
+            y = max_y * (i / n_grid) + margin
+            for j in range(n_grid + 1):
+                x = max_x * (j / n_grid) + margin
+                roi = RegionOfInterest(x1=x, x2=x + box_width, y1=y, y2=y + box_height)
+                seg_roi = SegmentOfInterest(x=range(int(roi.x1), int(roi.x2)), y=range(int(roi.y1), int(roi.y2)))
+                detections.append(
+                    Detection(
+                        info=DetectionInfo(score=np.random.uniform(), class_id=-1, class_name="test"),
+                        roi=roi,
+                        seg_roi=seg_roi
+                    )
+                )
+        return detections
 
 
 def _get_detections_for_topic():
---
+++ src/rasberry_perception/detection/visualisation.py
@@ -0,0 +1,254 @@
+#  Raymond Kirk (Tunstill) Copyright (c) 2020
+#  Email: ray.tunstill@gmail.com
+
+import colorsys
+
+import cv2
+import matplotlib.colors as mplc
+import numpy as np
+
+__all__ = ["Visualiser"]
+
+
+_COLORS = np.array([0.000, 0.447, 0.741, 0.850, 0.325, 0.098, 0.929, 0.694, 0.125, 0.494, 0.184, 0.556, 0.466, 0.674,
+                    0.188, 0.301, 0.745, 0.933, 0.635, 0.078, 0.184, 0.300, 0.300, 0.300, 0.600, 0.600, 0.600, 1.000,
+                    0.000, 0.000, 1.000, 0.500, 0.000, 0.749, 0.749, 0.000, 0.000, 1.000, 0.000, 0.000, 0.000, 1.000,
+                    0.667, 0.000, 1.000, 0.333, 0.333, 0.000, 0.333, 0.667, 0.000, 0.333, 1.000, 0.000, 0.667, 0.333,
+                    0.000, 0.667, 0.667, 0.000, 0.667, 1.000, 0.000, 1.000, 0.333, 0.000, 1.000, 0.667, 0.000, 1.000,
+                    1.000, 0.000, 0.000, 0.333, 0.500, 0.000, 0.667, 0.500, 0.000, 1.000, 0.500, 0.333, 0.000, 0.500,
+                    0.333, 0.333, 0.500, 0.333, 0.667, 0.500, 0.333, 1.000, 0.500, 0.667, 0.000, 0.500, 0.667, 0.333,
+                    0.500, 0.667, 0.667, 0.500, 0.667, 1.000, 0.500, 1.000, 0.000, 0.500, 1.000, 0.333, 0.500, 1.000,
+                    0.667, 0.500, 1.000, 1.000, 0.500, 0.000, 0.333, 1.000, 0.000, 0.667, 1.000, 0.000, 1.000, 1.000,
+                    0.333, 0.000, 1.000, 0.333, 0.333, 1.000, 0.333, 0.667, 1.000, 0.333, 1.000, 1.000, 0.667, 0.000,
+                    1.000, 0.667, 0.333, 1.000, 0.667, 0.667, 1.000, 0.667, 1.000, 1.000, 1.000, 0.000, 1.000, 1.000,
+                    0.333, 1.000, 1.000, 0.667, 1.000, 0.333, 0.000, 0.000, 0.500, 0.000, 0.000, 0.667, 0.000, 0.000,
+                    0.833, 0.000, 0.000, 1.000, 0.000, 0.000, 0.000, 0.167, 0.000, 0.000, 0.333, 0.000, 0.000, 0.500,
+                    0.000, 0.000, 0.667, 0.000, 0.000, 0.833, 0.000, 0.000, 1.000, 0.000, 0.000, 0.000, 0.167, 0.000,
+                    0.000, 0.333, 0.000, 0.000, 0.500, 0.000, 0.000, 0.667, 0.000, 0.000, 0.833, 0.000, 0.000, 1.000,
+                    0.000, 0.000, 0.000, 0.143, 0.143, 0.143, 0.857, 0.857, 0.857, 1.000, 1.000, 1.000]
+                   ).astype(np.float32).reshape(-1, 3)
+
+
+def random_color(rgb=False, maximum=255):
+    """
+    Args:
+        rgb (bool): whether to return RGB colors or BGR colors.
+        maximum (int): either 255 or 1
+
+    Returns:
+        ndarray: a vector of 3 numbers
+    """
+    idx = np.random.randint(0, len(_COLORS))
+    ret = _COLORS[idx] * maximum
+    if not rgb:
+        ret = ret[::-1]
+    return ret
+
+
+class GenericMask:
+    def __init__(self, mask_or_polygons, height, width):
+        self._mask = self._polygons = None
+        self.height = height
+        self.width = width
+
+        m = mask_or_polygons
+
+        if isinstance(m, list):
+            self._polygons = [np.asarray(x).reshape(-1) for x in m]
+            return
+
+        if isinstance(m, np.ndarray):  # assumed to be a binary mask
+            assert m.shape[1] != 2, m.shape
+            assert m.shape == (height, width), m.shape
+            self._mask = m.astype("uint8")
+            return
+
+        raise ValueError("GenericMask cannot handle object {} of type '{}'".format(m, type(m)))
+
+    @property
+    def polygons(self):
+        if self._polygons is None:
+            mask = np.ascontiguousarray(self._mask)
+            res = cv2.findContours(mask.astype("uint8"), cv2.RETR_CCOMP, cv2.CHAIN_APPROX_NONE)
+            hierarchy = res[-1]
+            if hierarchy is None:  # empty mask
+                return [], False
+            has_holes = (hierarchy.reshape(-1, 4)[:, 3] >= 0).sum() > 0
+            res = res[-2]
+            res = [x.flatten() for x in res]
+            res = [x for x in res if len(x) >= 6]
+            self._polygons = res
+        return self._polygons
+
+
+class Visualiser:
+    def __init__(self, img_rgb):
+        self.img = np.asarray(img_rgb).clip(0, 255).astype(np.uint8)
+        self.height, self.width = self.img.shape[:2]
+
+        # Layers of the final visualisation
+        self._overlay = np.zeros(self.img.shape) - 1  # -1 is the invalid overlay value
+        self._text = np.zeros(self.img.shape) - 1
+
+        self._min_text_height = 10
+        self._max_text_height = self.img.shape[0] // 4
+
+    def draw_detections_message(self, message):
+        boxes = []
+        masks = []
+        labels = []
+        assigned_colors = None
+        alpha = 0.5
+
+        for detection in message.detections.detections:
+            boxes.append([detection.roi.x1, detection.roi.y1, detection.roi.x2, detection.roi.y2])
+            xyxy_abs = list(zip(detection.seg_roi.x, detection.seg_roi.y))
+            masks.append(GenericMask([xyxy_abs], self.height, self.width))
+            labels.append("{} {:.2f}".format(detection.info.class_name, detection.info.score))
+
+        self.overlay_instances(boxes, labels, masks, assigned_colors, alpha)
+
+    def overlay_instances(self, boxes=None, labels=None, masks=None, assigned_colors=None, alpha=0.5):
+        num_instances = None
+        if boxes is not None:
+            boxes = self._convert_boxes(boxes)
+            num_instances = len(boxes)
+        if masks is not None:
+            masks = self._convert_masks(masks)
+            if num_instances:
+                assert len(masks) == num_instances
+            else:
+                num_instances = len(masks)
+
+        if labels is not None:
+            assert len(labels) == num_instances
+        if assigned_colors is None:
+            assigned_colors = [random_color(rgb=True, maximum=1) for _ in range(num_instances)]
+        if num_instances == 0:
+            return
+
+        # Display largest to smallest (less occlusion)
+        areas = None
+        if boxes is not None:
+            areas = np.prod(boxes[:, 2:] - boxes[:, :2], axis=1)
+
+        if areas is not None:
+            sorted_idxs = np.argsort(-areas).tolist()
+            boxes = boxes[sorted_idxs] if boxes is not None else None
+            labels = [labels[k] for k in sorted_idxs] if labels is not None else None
+            masks = [masks[idx] for idx in sorted_idxs] if masks is not None else None
+            assigned_colors = [assigned_colors[idx] for idx in sorted_idxs]
+
+        for i in range(num_instances):
+            color = assigned_colors[i]
+
+            if boxes is not None:
+                self.draw_box(boxes[i], edge_color=color)
+
+            if masks is not None:
+                for segment in masks[i].polygons:
+                    self.draw_polygon(segment.reshape(-1, 2), color, alpha=alpha)
+
+            if labels is not None:
+                lighter_color = self._change_color_brightness(color, brightness_factor=0.7)
+                self.draw_text_for_box(labels[i], bbox=boxes[i], font_scale=1.0 / 8.0, color=lighter_color, thickness=2)
+
+    def get_image(self, overlay_alpha=0.5):
+        canvas = self.img.copy()
+        overlay_valid = np.where(self._overlay != -1)
+        canvas[overlay_valid] = (canvas[overlay_valid] * (1 - overlay_alpha)) + (self._overlay[overlay_valid] *
+                                                                                 overlay_alpha)
+        text_valid = np.where(self._text != -1)
+        canvas[text_valid] = self._text[text_valid]
+        return canvas
+
+    """
+    Primitive drawing functions:
+    """
+    def draw_text_for_box(self, text, bbox, font_scale=1.0, color=None, origin="above top left", thickness=1):
+        # If box height is passed scale relative to box height * font_scale
+        font = cv2.FONT_HERSHEY_COMPLEX
+
+        x0, y0, x1, y1 = bbox
+        bbox_height = y1 - y0
+        text_height = min(max(self._min_text_height, bbox_height * font_scale), self._max_text_height)
+        font_scale = cv2.getFontScaleFromHeight(font, int(text_height), thickness)
+        (label_width, label_height), baseline = cv2.getTextSize(text, font, font_scale, thickness)
+        pad = 0.7
+        box_pad = min(int((1 - pad) * label_width), int((1 - pad) * label_height))
+        label_height += box_pad
+        position = (x0 if "left" in origin else x1, y0 if "top" in origin else y1 - label_height - box_pad)
+        if "above" in origin:
+            position = (position[0], position[1] - label_height - box_pad)
+        elif "below" in origin:
+            position = (position[0], position[1] + label_height + box_pad)
+        self.draw_text(text, position, font_scale, color, font, thickness)
+
+    def draw_text(self, text, position, font_scale=1.0, color=None, font=None, thickness=1):
+        if color is None:
+            color = np.asarray((1.0, 1.0, 1.0))
+        color = np.maximum(list(mplc.to_rgb(color)), 0.2)
+        color[np.argmax(color)] = max(0.8, np.max(color))
+        color = tuple(int(i * 255) for i in color)
+        position = tuple(int(i) for i in position)
+
+        if font is None:
+            font = cv2.FONT_HERSHEY_COMPLEX
+
+        scale = font_scale
+        (label_width, label_height), baseline = cv2.getTextSize(text, font, scale, thickness)
+
+        pad = 0.7
+        box_pad = min(int((1 - pad) * label_width), int((1 - pad) * label_height))
+        box_cords = (position[0], position[1],
+                     position[0] + label_width + (box_pad * 2), position[1] + label_height + (box_pad * 2))
+        self.draw_box(box_cords, np.asarray((0, 0, 0)), fill=True)
+
+        position = (position[0] + box_pad, position[1] + box_pad + label_height)
+        cv2.putText(self._text, text, position, font, scale, color, thickness, cv2.LINE_AA)
+
+    def draw_box(self, box_coord, edge_color, fill=False):
+        x0, y0, x1, y1 = [int(i) for i in box_coord]
+        line_width = -1 if fill else 2
+        edge_color = list((edge_color * 255).astype(np.int))
+        cv2.rectangle(self._overlay, (x0, y0), (x1, y1), edge_color, line_width)
+
+    def draw_polygon(self, segment, color, edge_color=None, alpha=0.5):
+        if edge_color is None:
+            # Edge is brighter
+            edge_color = np.asarray(self._change_color_brightness(color, brightness_factor=0.7))
+
+        color = list((color * 255).astype(int))
+        edge_color = list((edge_color * 255.0).astype(int))
+        edge_thickness = 2
+        segment = segment.reshape((-1, 1, 2))
+        cv2.fillPoly(self._overlay, segment, color)
+        cv2.polylines(self._overlay, segment, False, edge_color, edge_thickness)
+
+    """
+    Internal methods:
+    """
+    def _change_color_brightness(self, color, brightness_factor):
+        assert brightness_factor >= -1.0 and brightness_factor <= 1.0
+        color = mplc.to_rgb(color)
+        polygon_color = colorsys.rgb_to_hls(*mplc.to_rgb(color))
+        modified_lightness = polygon_color[1] + (brightness_factor * polygon_color[1])
+        modified_lightness = 0.0 if modified_lightness < 0.0 else modified_lightness
+        modified_lightness = 1.0 if modified_lightness > 1.0 else modified_lightness
+        modified_color = colorsys.hls_to_rgb(polygon_color[0], modified_lightness, polygon_color[2])
+        return modified_color
+
+    def _convert_boxes(self, boxes):
+        return np.asarray(boxes)
+
+    def _convert_masks(self, masks_or_polygons):
+        m = masks_or_polygons
+        ret = []
+        for x in m:
+            if isinstance(x, GenericMask):
+                ret.append(x)
+            else:
+                ret.append(GenericMask(x, self.height, self.width))
+        return ret
+
+
