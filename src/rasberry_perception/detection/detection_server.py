975a20e10138653a14ba8484823ae58997c2990b
RaymondKirk <ray.tunstill@live.co.uk>
2020-03-06 13:46:55 +0000

    Added command line argument preference for detection_server.py

--- src/rasberry_perception/detection/detection_server.py
+++ src/rasberry_perception/detection/detection_server.py
@@ -5,10 +5,32 @@
 
 # Executable for running the detection service server backend from rosrun
 
+import argparse
+
+from rasberry_perception.detection import Server
+
+
+def _default_arg_parser(args=None):
+    parser = argparse.ArgumentParser(description='Run the detection server.')
+    parser.add_argument('--backend', type=str, help="Which backend to use.", default=None)
+    parsed_args, unknown = parser.parse_known_args() #this is an 'internal' method
+
+    # Add unrecognised args as kwargs for passing the detection server
+    unknown_parser = argparse.ArgumentParser()
+    for arg in unknown:
+        if arg.startswith(("-", "--")):
+            unknown_parser.add_argument(arg, type=str)
+    unknown_args = unknown_parser.parse_args(unknown)
+    unknown_kwargs = {a: getattr(unknown_args, a) for a in vars(unknown_args)} if len(vars(unknown_args)) else None
+
+    return parsed_args, unknown_kwargs
+
 
 def __detection_server_runner():
-    from rasberry_perception.detection import Server
-    server = Server()
+    # Command line arguments should always over ride ros parameters
+    args, args_kwargs = _default_arg_parser()
+
+    server = Server(backend=args.backend, backend_kwargs=args_kwargs)
     server.run()
 
 
