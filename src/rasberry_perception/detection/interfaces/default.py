418dda545f5a83def987dbc140220b6d408b0172
RaymondKirk <ray.tunstill@live.co.uk>
2020-03-06 13:46:34 +0000

    Added limiter to default server

--- src/rasberry_perception/detection/interfaces/default.py
+++ src/rasberry_perception/detection/interfaces/default.py
@@ -17,16 +17,25 @@ class BaseDetectionServer:
 
     The .___init___() method should be overridden and any interface initialisation done here such as loading models.
         Service name should not be changed unless multiple servers need to be run.
-        Classes that inherit must call this baseclass at the end of their __init__ method.
+        Classes that inherit must call this baseclass at the end of their __init__ method or spin=False and call spin
+        manually.
     The .get_detector_results() method must be overridden, this method should call the interface and translate the
         results to be in the rasberry_perception.msg.Detections message format.
+
+    Args:
+        service_name (str): Name of the created service. Should not be changed unless multiple servers need to be run.
+        spin (bool): If True the service spin method called on construction. If false .spin() must be called later.
     """
-    def __init__(self, service_name=default_service_name):
+    def __init__(self, service_name=default_service_name, spin=True):
         self.service_name = service_name
         self.currently_busy = Event()
         rospy.loginfo("Creating service {}".format(service_name))
         self.service_server = rospy.Service(service_name, GetDetectorResults, self.get_detector_results)
         rospy.loginfo("Waiting for requests on {}".format(service_name))
+        if spin:
+            self.spin()
+
+    def spin(self):
         self.service_server.spin()
 
     def get_detector_results(self, request):
@@ -38,14 +47,19 @@ class DefaultDetectionServer(BaseDetectionServer):
     """Example of how to define a detection server interface.
 
     Every interface must register itself in the DETECTION_REGISTRY so ros is aware of it.
+
+    Args:
+        rate: The service will wait 1 / rate seconds before returning the default message
     """
-    def __init__(self):
+    def __init__(self, rate=30):
         # Extra initialisation done here
         status_msg = DetectionStatus(OKAY=True, ERROR=False, BUSY=False)
         self.default_response = GetDetectorResultsResponse(status=status_msg)
-        # Base class must be called at the end due to self.service_se
+        self._hz_limit = rospy.Rate(rate)
+        # Base class must be called at the end due to self.service_server.spin()
         BaseDetectionServer.__init__(self)
 
     @function_timer.interval_logger(interval=10)
     def get_detector_results(self, request):
+        self._hz_limit.sleep()
         return self.default_response
