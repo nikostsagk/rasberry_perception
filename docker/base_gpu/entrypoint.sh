3db87d4c2e9b8fc4cef54b97f751ab13c388b995
Nikolaus Wagner <e11722950@student.tuwien.ac.at>
2020-03-10 15:47:27 +0000

    fix: Unify entrypoints for all backends

--- docker/base_gpu/entrypoint.sh
+++ docker/base_gpu/entrypoint.sh
@@ -2,4 +2,10 @@
 set -e
 source "/opt/ros/melodic/setup.bash"
 source "/catkin_ws/devel/setup.bash"
-exec "$@"
+{
+	echo "Try starting detection backend..."
+  source "/start_backend.sh" 2> /dev/null
+} || {
+  echo "No backend configured."
+  exec "$@"
+}
--- docker/detectron2/start_backend.sh
+++ docker/detectron2/start_backend.sh
@@ -8,5 +8,3 @@ source "/catkin_ws/devel/setup.bash"
 # RUN detectron2 backend
 source "backend_venv/bin/activate" && \
 rosrun rasberry_perception detection_server.py --backend detectron2 --config-file /detectron2/configs/COCO-Detection/fast_rcnn_R_50_FPN_1x.yaml
-
-exec "$@"
