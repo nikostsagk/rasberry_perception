5fb4984b3f56b4f999f0ff64d689319686d6d6ee
RaymondKirk <ray.tunstill@live.co.uk>
2020-03-11 18:44:53 +0000

    Added track counter file (for new interface)

--- src/rasberry_perception/tracking/count_unique_tracks.py
+++ src/rasberry_perception/tracking/count_unique_tracks.py
@@ -2,36 +2,38 @@
 #  Email: ray.tunstill@gmail.com
 
 import rospy
-from bayes_people_tracker.msg import PeopleTracker
+from rasberry_perception.msg import TrackerResults
+
 from std_msgs.msg import String
 
 
 class CountAnnouncer:
     def __init__(self, people_tracker_topic=None, reset_topic=None):
         # Counter publisher
-        self.count_publisher = rospy.Publisher("/rasberry_perception/tracker/unique_count", String, queue_size=1)
+        self.count_publisher = rospy.Publisher("/rasberry_perception/tracking/unique_count", String, queue_size=1)
 
         # Subscribers
-        self.uuids = set()
+        self.labels = set()
         if people_tracker_topic is None:
-            people_tracker_topic = 'rasberry_perception/tracker/positions'
-        self.positions_publisher = rospy.Subscriber(people_tracker_topic, PeopleTracker, self._track_callback)
+            people_tracker_topic = '/rasberry_perception/tracking/results_array'
+        self.tracker_results_subscriber = rospy.Subscriber(people_tracker_topic, TrackerResults, self._track_callback)
 
         if reset_topic is not None:
             self.reset_on = rospy.Subscriber(reset_topic, String, callback=self._reset)
 
     def _reset(self, *args):
-        self.uuids.clear()
-        self.count_publisher.publish(String(str(len(self.uuids))))
+        self.labels.clear()
+        self.count_publisher.publish(String(str(len(self.labels))))
 
-    def _track_callback(self, detection_message):
-        for uuid in detection_message.uuids:
-            self.uuids.add(uuid)
-        self.count_publisher.publish(String(str(len(self.uuids))))
+    def _track_callback(self, tracker_results_msg):
+        for track in tracker_results_msg.tracks:
+            self.labels.add(track.id)
+        self.count_publisher.publish(String(str(len(self.labels))))
+        print(str(len(self.labels)))
 
 
 def __count_unique_tracks():
-    rospy.init_node('bayes_people_track_counter', anonymous=True)
+    rospy.init_node('rasberry_tracking_counter', anonymous=True)
     tracker_topic = rospy.get_param("~tracker_topic", None)
     reset_topic = rospy.get_param("~reset_topic", "/sequence_0/info")
     counter = CountAnnouncer(tracker_topic, reset_topic)
