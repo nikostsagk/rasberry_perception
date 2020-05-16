f8084fc3bfe83028b4806991b146e3bfba035582
RaymondKirk <ray.tunstill@live.co.uk>
2020-05-16 17:02:07 +0100

    Fixed base_gpu docker for melodic

--- docker/base_gpu/entrypoint.sh
+++ docker/base_gpu/entrypoint.sh
@@ -3,9 +3,9 @@ set -e
 source "/opt/ros/melodic/setup.bash"
 source "/catkin_ws/devel/setup.bash"
 {
-	echo "Try starting detection backend..."
-  source "/start_backend.sh" 2> /dev/null
+    echo "Try starting detection backend..."
+    source "/start_backend.sh" 2> /dev/null
 } || {
-  echo "No backend configured."
-  exec "$@"
+    echo "No backend configured."
+    exec "$@"
 }
