c905cf0059edcb75d3ae7f58b4e8e6f96ee660b6
RaymondKirk <ray.tunstill@live.co.uk>
2020-05-18 14:29:12 +0100

    Fixes for GPU docker backend and detectron2 backends

--- docker/detectron2/start_backend.sh
+++ docker/detectron2/start_backend.sh
@@ -2,5 +2,5 @@
 set -e
 
 # RUN detectron2 backend
-source "backend_venv/bin/activate" && \
+source "/home/appuser/detectron2_venv/bin/activate" && \
 rosrun rasberry_perception detection_server.py --backend detectron2 --config-file /r50_packaged/config.yaml
