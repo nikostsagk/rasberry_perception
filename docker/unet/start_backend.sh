26e340101360756bfa0c6613348a20c10850e481
saul <saul.goldblatt@sagarobotics.com>
2021-06-24 18:06:19 +0100

    udpated docker entrypoints to allow for multiple services

--- docker/unet/start_backend.sh
+++ docker/unet/start_backend.sh
@@ -2,5 +2,5 @@
 set -e
 
 source "/unet_venv/bin/activate" && \
-rosrun rasberry_perception detection_server.py --backend unet --model_path epoch_80.pth --config_path param.yaml
+rosrun rasberry_perception detection_server.py --backend unet --model_path epoch_80.pth --config_path param.yaml --service_name gripper_perception
 #echo 'here3'
