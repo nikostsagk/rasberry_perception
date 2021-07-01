69714a1500080f4aa16c2893148ba7f38ca9ad70
nikostsagk <n.tsagko@gmail.com>
2021-07-01 19:02:25 +0100

    Introduce some modularity using docker-compose

--- docker/detectron2_jetson/start_backend.sh
+++ docker/detectron2_jetson/start_backend.sh
@@ -3,4 +3,4 @@ set -e
 
 # RUN detectron2 backend
 source "/detectron2_venv/bin/activate" && \
-rosrun rasberry_perception detection_server.py --backend detectron2 --config_file detectron2_repo/configs/COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml --model_file model_final.pth --service_name gripper_perception
+rosrun rasberry_perception detection_server.py --backend detectron2 --config_file config.yaml --model_file model.pth --service_name $SERVICE_NAME
