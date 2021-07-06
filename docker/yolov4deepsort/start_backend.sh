cf3fde5be1c9570a8c393f771241081305af995a
saul <saul.goldblatt@sagarobotics.com>
2021-07-06 16:54:50 +0100

    added service_name to start_backend.sh

--- docker/yolov4deepsort/start_backend.sh
+++ docker/yolov4deepsort/start_backend.sh
@@ -2,4 +2,4 @@
 set -e
 
 # RUN yolov4deepsort backend
-rosrun rasberry_perception detection_server.py --backend yolov4deepsort --configPath /catkin_ws/src/Noronn/deep_sort_yolov4-master/cfg/yolov4_sb.cfg --metaPath /catkin_ws/src/Noronn/deep_sort_yolov4-master/cfg/voc_sb.data
+rosrun rasberry_perception detection_server.py --backend yolov4deepsort --configPath /catkin_ws/src/Noronn/deep_sort_yolov4-master/cfg/yolov4_sb.cfg --metaPath /catkin_ws/src/Noronn/deep_sort_yolov4-master/cfg/voc_sb.data --service_name robot_perception
