62079c71f6d9a6acef99ea45b47b592a85ac8238
nikostsagk <n.tsagko@gmail.com>
2021-07-07 15:41:59 +0100

    Fix conflicts

--- docker/gpu/detectron2/start_backend.sh
+++ docker/gpu/detectron2/start_backend.sh
@@ -3,4 +3,4 @@ set -e
 
 # RUN detectron2 backend
 source "/detectron2_venv/bin/activate" && \
-rosrun rasberry_perception detection_server.py --backend detectron2 --config_file config.yaml --model_file model.pth --service_name gripper_perception
+rosrun rasberry_perception detection_server.py --backend detectron2 --config_file config.yaml --model_file model.pth --service_name $SERVICE_NAME
