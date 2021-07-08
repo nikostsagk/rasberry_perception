4205224743532310a648f8180da8ebe9e83d615e
Rob <rbelshaw@sagarobotics.com>
2021-07-08 01:06:09 +0100

    WIP

--- docker/gpu/tensorrtdeepsort/start_backend.sh
+++ docker/gpu/tensorrtdeepsort/start_backend.sh
@@ -4,4 +4,4 @@ source "/opt/ros/melodic/setup.bash"
 source "/catkin_ws/devel/setup.bash"
 # RUN tensorrtdeepsort backend
 source "modularmot_venv/bin/activate" && \
-rosrun rasberry_perception detection_server.py --backend tensorrtdeeposrt --configPath /ModularMOT/cfg/mot.json 
+rosrun rasberry_perception detection_server.py --backend tensorrtdeeposrt --config_path /ModularMOT/cfg/mot.json --service_name get_detections_service
--- docker/jetson/tensorrtdeepsort/start_backend.sh
+++ docker/jetson/tensorrtdeepsort/start_backend.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 set -e
-
-# RUN yolov4deepsort backend
-rosrun rasberry_perception detection_server.py --backend tensorrtdeeposrt --configPath /catkin_ws/src/Noronn/deep_sort_yolov4-master/cfg/yolov4_sb.cfg
+source "/opt/ros/melodic/setup.bash"
+source "/catkin_ws/devel/setup.bash"
+# RUN tensorrtdeepsort backend
+source "modularmot_venv/bin/activate" && \
+rosrun rasberry_perception detection_server.py --backend tensorrtdeeposrt --config_path /ModularMOT/cfg/mot.json --service_name get_detections_service
