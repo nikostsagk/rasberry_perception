70268b8a50ebd3bee13fd90671d863218b7dc462
rjwb1 <rbelshaw@sagarobotics.com>
2021-07-22 09:51:01 +0100

    docker improvements

--- docker/jetson/tensorrtdeepsort/start_backend.sh
+++ docker/jetson/tensorrtdeepsort/start_backend.sh
@@ -4,4 +4,4 @@ source "/opt/ros/melodic/setup.bash"
 source "/catkin_ws/devel/setup.bash"
 # RUN tensorrtdeepsort backend
 source "modularmot_venv/bin/activate" && \
-rosrun rasberry_perception detection_server.py --backend tensorrtdeepsort --config_path /ModularMOT/cfg/mot.json --service_name get_detections_service
+rosrun rasberry_perception detection_server.py --backend tensorrtdeepsort --config_path /PerceptionApps/ModularMOT/cfg/tensorrtdeepsort.json --service_name robot_perception
