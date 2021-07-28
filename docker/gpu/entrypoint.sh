8c5d82eb6e544866a9d3a43d07a10c0861e19726
Nikos Tsagkopoulos <n.tsagko@gmail.com>
2021-07-28 17:03:55 +0100

    fix typo in entrypoint.sh

--- docker/gpu/entrypoint.sh
+++ docker/gpu/entrypoint.sh
@@ -4,7 +4,7 @@ source "/opt/ros/melodic/setup.bash"
 source "/catkin_ws/devel/setup.bash"
 {
     echo "Try starting detection backend..."
-    source "/start_backend.sh" 2
+    source "/start_backend.sh" 2> /dev/null
 } || {
     echo "No backend configured."
     exec "$@"
